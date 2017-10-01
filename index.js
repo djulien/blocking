8088ddeecd28ec94653a3996fa98ceb8964d9d50
dj <djulien17@thejuliens.net>
2017-09-30 17:09:43 -0700

    add methods for pause, step

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 const keypress = require('keypress');
 //const {caller} = require("./caller");
 //const {debug} = require('./debug');
-
+const util = require("util");
 
 //register callbacks when synchronous main ends:
 const onexit =
@@ -23,35 +23,50 @@ function onexit(new_cb)
 }
 
 
-//step thru generator function:
+//step thru generator function until done:
 //allows synchronous (blocking) coding style
+//returns to caller immediately
 const blocking =
 module.exports.blocking =
 function blocking(gen)
 {
-	if (typeof gen == "function") //invoke generator if not already
-    {
-//console.log("blocking: func -> gen");
-        setImmediate(function() { blocking(gen()); }); //avoid hoist errors
-        return;
-    }
 //    process.stdin.pause(); //don't block process exit while not waiting for input
 //    var retval;
 	for (;;)
 	{
-console.log("prev retval", typeof blocking.retval, JSON.stringify(blocking.retval));
-		var status = gen.next(blocking.retval); //send previous value to generator
-//		if (!status.done && (typeof status.value == "undefined")) continue; //cooperative multi-tasking
-		if (typeof status.value == "function") //caller wants manual step
-        {
-            blocking.retval = status.value(gen); //remember latest ret val
-            return; //caller wants to execute before next step
-        }
-        blocking.retval = status.value; //remember latest ret val, pass to next step
-		if (!status.done) continue;
+        var done = step(gen);
+        if (typeof done == "undefined") return;
+		if (!done) continue;
         onexit();
-        return blocking.retval;
+        return step.retval;
 	}
+}
+
+
+//step thru generator function once:
+//allows synchronous (blocking) coding style
+const step =
+module.exports.step =
+function step(gen)
+{
+	if (typeof gen == "function") //invoke generator if not already
+    {
+//console.log("blocking: func -> gen");
+        setImmediate(function() { step(gen()); }); //avoid hoist errors
+        return;
+    }
+//    process.stdin.pause(); //don't block process exit while not waiting for input
+//    var retval;
+//console.log("prev retval", typeof blocking.retval, util.inspect(blocking.retval));
+	var status = gen.next(step.retval); //send previous value to generator
+//		if (!status.done && (typeof status.value == "undefined")) continue; //cooperative multi-tasking
+	if (typeof status.value == "function") //caller wants manual step
+    {
+        step.retval = status.value(gen); //remember latest ret val
+        return; //caller wants to execute before next step
+    }
+    step.retval = status.value; //remember latest ret val, pass to next step
+    return status.done;
 }
 
 
@@ -63,6 +78,15 @@ function wait(delay)
 //console.log("%swait(%d sec)", (delay <= 0)? "no-": "", delay);
     delay *= 1000; //sec -> msec
     return (delay > 0)? setTimeout.bind(null, blocking, delay): setImmediate.bind(null, blocking);
+}
+
+
+//allow caller to pause and restart later:
+const pause =
+module.exports.pause =
+function pause(THIS)
+{
+    return function(gen) { /*debug("save gen", gen)*/; THIS.svgen = gen; } //save generator for text wakeup later
 }
 
 
@@ -84,13 +108,13 @@ function prompt(args)
 //        process.exit();
 //        process.stdin.pause();
             blocking.retval = text.toString(); //give entered text back to caller
-            blocking(prompt.gen);
+            blocking(prompt.svgen);
         });
         prompt.init = true;
     }
     process.stdin.resume();
     console.log.apply(console, args? arguments: ["Enter to continue ..."]);
-    return function(gen) { prompt.gen = gen; } //save generator for text wakeup later
+    return pause(prompt); //function(gen) { prompt.gen = gen; } //save generator for text wakeup later
 }
 
 
@@ -110,14 +134,14 @@ function getchar(args)
 //        console.log('got "keypress"', key);
 //        if (key && key.ctrl && key.name == 'c') process.stdin.pause();
             blocking.retval = (key || {}).name;
-            blocking(getchar.gen);
+            blocking(getchar.svgen);
         });
         getchar.init = true;
     }
     process.stdin.resume();
     if (console.log._previous) console.log._previous = null; //kludge: defeat custom console dedup
     console.log.apply(console, args? arguments: ["Any key to continue ..."]);
-    return function(gen) { /*debug("save gen", gen)*/; getchar.gen = gen; } //save generator for text wakeup later
+    return pause(getchar); //function(gen) { /*debug("save gen", gen)*/; getchar.gen = gen; } //save generator for text wakeup later
 }
 
 //eof
